f2720bd28adb103f3ec4d246c790cc46805148a1
Prasannakumar55 <104211409+Prasannakumar55@users.noreply.github.com>
2022-07-07 19:43:03 +0530

    Add files via upload

---
+++ Asst8.js
@@ -0,0 +1,71 @@
+ 1.
+function Check(obj1){
+    obj1.setter=function(){
+       console.log(this.name);
+     }
+}
+
+2.
+function Check(obj1) {
+    delete obj1.rollno;
+     return obj1;
+ }
+ 
+ 3.
+ function Check(obj1) {
+    if (obj1.salary<500000){
+      return "NotDream"
+    }
+    else {
+     return "Dream"
+    }
+}
+
+4.
+
+function Check(obj1) {
+    if(Object.keys(obj1).length === 0){
+      return "false"
+    }
+    else {
+      return "true"
+    }
+ }
+ 
+ 
+5.
+ 
+function Check(obj1,obj2) {
+    let sum=Object.assign(obj1,obj2);
+    return sum;
+}
+ 
+6.
+function Check(a,obj1) {
+    obj1.id=obj1.id*a;
+        obj1.houseno=obj1.houseno*a;
+        return obj1;
+}
+
+7.
+function Check(obj1) {
+    let sum=obj1.p1+obj1.p2+obj1.p3;
+    return sum;
+}
+
+8.
+function check(obj1,a,b) {
+    if((obj1.name ==a)&&(obj1.id ==b)){
+      return ("true");
+    }
+    else {
+      return ("false");
+    }
+}
+
+
+
+
+
+    
+    
